ba29908ff60d8d52bc6f1b7efb30ba0c893cfb5f
gsingh737 <ssgurpreetsingh@gmail.com>
2016-12-18 00:33:10 -0500

    Testing commit

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 // app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) = {
-    if(err){
-      console.log('Unable to log to server.log');
-    }
-  });
-  next();
-});
-
-app.use((req, res, next) => {
- res.render('maintenance.hbs');
-});
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
